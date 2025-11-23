1b9a864fd7d163086eae8c0baa5d2c1e24dbb377
Artem-1-1 <aleksejchukartem@gmail.com>
2025-11-23 11:21:04 +0300

    Added 3 methods to Tree class.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-class Node {
+leftChildclass Node {
   constructor(data) {
     this.data = data,
-    this.left = null;
-    this.right = null; 
+    this.leftChild = null;
+    this.rightChild = null; 
   }
 }
 
@@ -17,8 +17,44 @@ class Tree {
 
     const midPoint = Math.floor(sortedArray.length / 2);
     const newNode = Node(sortedArray[midPoint]);
-    newNode.left = this.buildTree(sortedArray.slice(0, midPoint));
-    newNode.right = this.buildTree(sortedArray.slice(midPoint + 1));
+    newNode.leftChild = this.buildTree(sortedArray.slice(0, midPoint));
+    newNode.rightChild = this.buildTree(sortedArray.slice(midPoint + 1));
     return newNode;
   }
+
+  insert(value, currentNode = this.root) {
+    if (currentNode === null) return Node(value);
+    if (currentNode.value === value) return;
+
+    if (currentNode.value < value) {
+      currentNode.rightChild = this.insert(value, currentNode.right);
+    } else {
+      currentNode.leftChild = this.insert(value, currentNode.leftChild);
+    }
+    return currentNode;
+  }
+
+  remove(value, currentNode = this.root) {
+    if (currentNode === null) return currentNode;
+
+    if (currentNode === value) return;
+    else if (currentNode.value > value) {
+      currentNode.leftChild = this.remove(value, currentNode.leftChild);
+    } else {
+      currentNode.rightChild = this.remove(value, currentNode.right);
+    }
+    return currentNode;
+  }
+
+  find(value, node = this.root) {
+    if (node === null || node.value === value) return;
+
+    if(node.value < value) {
+      return this.find(value, node.rightChild)
+    } else  {
+      return this.find(value, node.leftChild);
+    }   
+  }
+
+
 }
