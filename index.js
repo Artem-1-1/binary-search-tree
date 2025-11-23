42d1aec02a5c5b17c5218a74196fb81dbd50395f
Artem-1-1 <aleksejchukartem@gmail.com>
2025-11-23 10:37:22 +0300

    Added Tree class with buildTree method.

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ class Node {
     this.right = null; 
   }
 }
+
+class Tree {
+  constructor(array) {
+    const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+    this.root = this.buildTree(sortedArray)
+  }
+
+  buildTree(sortedArray) {
+    if (sortedArray.length === 0) return null;
+
+    const midPoint = Math.floor(sortedArray.length / 2);
+    const newNode = Node(sortedArray[midPoint]);
+    newNode.left = this.buildTree(sortedArray.slice(0, midPoint));
+    newNode.right = this.buildTree(sortedArray.slice(midPoint + 1));
+    return newNode;
+  }
+}
